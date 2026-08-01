04425b5eb4a88de1e70dd7d2165f9e4e35d3c81c
premanathvk <premanath584@gmail.com>
2026-08-01 17:43:14 +0530

    feat: Implemented service and repository layers

--- src/main/java/com/bank/transaction/processor/maveric/service/AccountService.java
+++ src/main/java/com/bank/transaction/processor/maveric/service/AccountService.java
@@ -1,147 +1,26 @@
 package com.bank.transaction.processor.maveric.service;
 
-import com.bank.transaction.processor.maveric.exception.*;
 import com.bank.transaction.processor.maveric.model.Account;
 import com.bank.transaction.processor.maveric.model.Transaction;
-import com.bank.transaction.processor.maveric.model.TransactionType;
 
 import java.math.BigDecimal;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
-public class AccountService {
+public interface AccountService {
 
-    private final Map<String, Account> accounts = new ConcurrentHashMap<>();
+    Account createAccount(String accountId, BigDecimal initialBalance);
 
-    public Account createAccount(String accountId, BigDecimal initialBalance) {
-        if (accounts.containsKey(accountId)) {
-            throw new AccountAlreadyExistsException(
-                    "Account already exists");
-        }
-        if (initialBalance.compareTo(BigDecimal.ZERO) <= 0) {
-            throw new InvalidAmountException("Deposit amount must be greater than zero");
-        }
+    Account deposit(String accountId, BigDecimal amount);
 
-        Account account = Account.builder()
-                .accountId(accountId)
-                .balance(initialBalance)
-                .build();
+    Account withdraw(String accountId, BigDecimal amount);
 
-        accounts.put(accountId, account);
+    void transfer(String fromAccountId,
+                  String toAccountId,
+                  BigDecimal amount);
 
-        return account;
-    }
+    BigDecimal getBalance(String accountId);
 
-    public Account deposit(String accountId, BigDecimal amount) {
+    List<Transaction> getTransactionHistory(String accountId);
 
-        if (amount == null || amount.compareTo(BigDecimal.ZERO) <= 0) {
-            throw new InvalidAmountException(
-                    "Deposit amount must be greater than zero");
-        }
-        Account account = getExistingAccount(accountId);
-
-        account.setBalance(account.getBalance().add(amount));
-        recordTransaction(account, TransactionType.DEPOSIT, amount, accountId, null, "Cash Deposit");
-        return account;
-    }
-
-    public Account withdraw(String accountId, BigDecimal amount) {
-
-        Account account = getExistingAccount(accountId);
-
-        if (amount == null || amount.compareTo(BigDecimal.ZERO) <= 0) {
-
-            throw new InvalidAmountException("Withdrawal amount must be greater than zero");
-        }
-        if (account.getBalance().compareTo(amount) < 0) {
-            throw new InsufficientFundsException(accountId);
-        }
-        account.setBalance(account.getBalance().subtract(amount));
-        recordTransaction(account, TransactionType.WITHDRAW, amount, accountId, null, "Cash Withdrawal");
-        return account;
-    }
-
-    private void recordTransaction(
-            Account account,
-            TransactionType type,
-            BigDecimal amount,
-            String fromAccountId,
-            String toAccountId,
-            String description) {
-
-        Transaction transaction = Transaction.builder()
-                .transactionType(type)
-                .amount(amount)
-                .fromAccountId(fromAccountId)
-                .toAccountId(toAccountId)
-                .balanceAfterTransaction(account.getBalance())
-                .description(description)
-                .build();
-
-        account.getTransactions().add(transaction);
-    }
-
-    public void transfer(String fromAccountId,
-                         String toAccountId,
-                         BigDecimal amount) {
-        Account source = getExistingAccount(fromAccountId);
-
-        Account destination = getExistingAccount(toAccountId);
-
-        if (amount == null ||
-                amount.compareTo(BigDecimal.ZERO) <= 0) {
-
-            throw new InvalidAmountException(
-                    "Transfer amount must be greater than zero");
-        }
-
-        if (source.getBalance().compareTo(amount) < 0) {
-            throw new InsufficientFundsException(fromAccountId);
-        }
-        if (fromAccountId.equals(toAccountId)) {
-            throw new InvalidTransferException();
-        }
-        source.setBalance(source.getBalance().subtract(amount));
-
-        destination.setBalance(destination.getBalance().add(amount));
-        recordTransaction(
-                source,
-                TransactionType.TRANSFER,
-                amount,
-                fromAccountId,
-                toAccountId,
-                "Transfer Sent");
-
-        recordTransaction(
-                destination,
-                TransactionType.TRANSFER,
-                amount,
-                fromAccountId,
-                toAccountId,
-                "Transfer Received");
-    }
-
-    private Account getExistingAccount(String accountId) {
-        Account account = accounts.get(accountId);
-        if (account == null) {
-            throw new AccountNotFoundException(accountId);
-        }
-        return account;
-    }
-
-    public Account getAccount(String accountId) {
-        return accounts.get(accountId);
-    }
-
-    public BigDecimal getBalance(String accountId) {
-        return getExistingAccount(accountId).getBalance();
-    }
-
-    public List<Transaction> getTransactionHistory(String accountId) {
-
-        return List.copyOf(
-                getExistingAccount(accountId)
-                        .getTransactions());
-    }
+    Account getAccount(String accountId);
 }
