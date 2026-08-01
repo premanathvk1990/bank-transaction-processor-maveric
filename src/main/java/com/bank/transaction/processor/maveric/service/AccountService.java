9c559d33130daed0e7d94ae7be3162d182ffffc9
premanathvk <premanath584@gmail.com>
2026-08-01 14:55:22 +0530

    feat: prevent duplicate account creation

--- src/main/java/com/bank/transaction/processor/maveric/service/AccountService.java
+++ src/main/java/com/bank/transaction/processor/maveric/service/AccountService.java
@@ -1,4 +1,5 @@
 package com.bank.transaction.processor.maveric.service;
+import com.bank.transaction.processor.maveric.exception.AccountAlreadyExistsException;
 import com.bank.transaction.processor.maveric.model.Account;
 
 import java.math.BigDecimal;
@@ -10,6 +11,10 @@ public class AccountService {
     private final Map<String, Account> accounts = new ConcurrentHashMap<>();
 
     public Account createAccount(String accountId, BigDecimal initialBalance) {
+        if (accounts.containsKey(accountId)) {
+            throw new AccountAlreadyExistsException(
+                    "Account already exists");
+        }
 
         Account account = Account.builder()
                 .accountId(accountId)
