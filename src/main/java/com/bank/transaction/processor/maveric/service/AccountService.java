ff553ed0668d558fd262958d803c070f6c055394
premanathvk <premanath584@gmail.com>
2026-08-01 16:12:12 +0530

    feat: add transfer method skeleton

--- src/main/java/com/bank/transaction/processor/maveric/service/AccountService.java
+++ src/main/java/com/bank/transaction/processor/maveric/service/AccountService.java
@@ -89,4 +89,10 @@ public class AccountService {
 
         account.getTransactions().add(transaction);
     }
+
+    public void transfer(String fromAccountId,
+                         String toAccountId,
+                         BigDecimal amount) {
+
+    }
 }
