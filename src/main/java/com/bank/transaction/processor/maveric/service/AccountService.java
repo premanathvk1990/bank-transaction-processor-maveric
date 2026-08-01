500898ebed627b18570a62b836a5ae9c1763c3e6
premanathvk <premanath584@gmail.com>
2026-08-01 14:46:28 +0530

    feat: implement account creation

---
+++ src/main/java/com/bank/transaction/processor/maveric/service/AccountService.java
@@ -0,0 +1,23 @@
+package com.bank.transaction.processor.maveric.service;
+import com.bank.transaction.processor.maveric.model.Account;
+
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class AccountService {
+
+    private final Map<String, Account> accounts = new ConcurrentHashMap<>();
+
+    public Account createAccount(String accountId, BigDecimal initialBalance) {
+
+        Account account = Account.builder()
+                .accountId(accountId)
+                .balance(initialBalance)
+                .build();
+
+        accounts.put(accountId, account);
+
+        return account;
+    }
+}
